eb225a22e02b21a0d18a4b8ee7fc7af1f94afb35
DieTime <gldenis00@mail.ru>
2020-10-28 12:46:35 +0300

    Getting files list

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <dirent.h>
+
+void print_dir(char* path);
 
 int main(int argc, char** argv) {
+    // Getting arguments
+    char* flag = argv[1];
+    char* path = argv[2];
+
     // Checking the number of arguments
     if (argc != 3) {
         printf("[ERROR] Program takes only 2 parameters: --pack(--unpack) and dir_path.");
@@ -10,19 +17,56 @@ int main(int argc, char** argv) {
     }
 
     // Checking the first argument
-    if (strcmp(argv[1], "--pack") != 0 && strcmp(argv[1], "--unpack") != 0) {
-        printf("[ERROR] Invalid argument: %s. Maybe you mean --pack or --unpack?", argv[1]);
+    if (strcmp(flag, "--pack") != 0 && strcmp(flag, "--unpack") != 0) {
+        printf("[ERROR] Invalid argument: %s. Maybe you mean --pack or --unpack?", flag);
         return 1;
     }
 
-    if (access(argv[2], F_OK) == -1) {
-        printf("[ERROR] Couldn't access %s. Maybe this path doesn't exists.", argv[2]);
+    // Check the directory path to access
+    if (access(path, F_OK) == -1) {
+        printf("[ERROR] Couldn't access %s. Maybe this path doesn't exists.", path);
         return 1;
     }
 
-    for (int i = 0; i < argc; ++i) {
-        printf("argv[%d]: %s\n", i, argv[i]);
-    }
+    // Get list of files in directory
+    print_dir(path);
 
     return 0;
+}
+
+void print_dir(char* path) {
+    // Open directory by path
+    DIR *dir = opendir(path);
+
+    // Stop if we couldn't open directory
+    if (dir == NULL) {
+        return;
+    }
+
+    // Get the next directory entry
+    struct dirent *entry;
+
+    // While the next directory entry exists
+    while ((entry = readdir(dir)) != NULL) {
+        // Skip if entry name is . or ..
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+
+        // Get current entry path
+        char file_path[1024] = {0};
+        snprintf(file_path, sizeof(file_path), "%s/%s", path, entry->d_name);
+
+        if (entry->d_type == DT_DIR) {
+            // Print entry files recursively if the entry is directory
+            print_dir(file_path);
+        }
+        else {
+            // Print entry path if the entry is file
+            printf("%s\n", file_path);
+        }
+    }
+
+    // Close directory
+    closedir(dir);
 }
