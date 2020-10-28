7cf89fed00e6c8d3dd6fd255ea317e85bb4e6e85
DieTime <gldenis00@mail.ru>
2020-10-28 11:34:22 +0300

    initialize git

---
+++ cmake-build-debug/CMakeFiles/tar.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tar.dir/main.c.obj"
+  "tar.exe"
+  "tar.exe.manifest"
+  "tar.lib"
+  "tar.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
